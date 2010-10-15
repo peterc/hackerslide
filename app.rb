e0d6d9df0005f2ee2221a5b33f87ae5d819dfef3
Peter Cooper <git@peterc.org>
2010-10-15 19:21:26 +0100

    Code cleanup and README improvements

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 get '/' do
-  # Get the dates
-  @dates = Dir[settings.data_root + '/*.json'].sort.reverse.first(70).reverse.map { |a| a[/\d{4}-\d{2}-\d{2}-\d*(?=\.)/] }.compact
+  # Get the dates. Be warned, I never said this was good code ;-)
+  @dates = Dir[settings.data_root + '/*.json'].sort.reverse.first(72).reverse.map { |a| a[/\d{4}-\d{2}-\d{2}-\d*(?=\.)/] }.compact
   erb :index
 end
