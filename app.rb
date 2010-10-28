a366463dcabda920288a2ea85f4aa69399940d78
Peter Cooper <peter@petercooper.co.uk>
2010-10-28 02:38:21 +0000

    Changed from 3 days to 1 week

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 get '/' do
   # Get the dates. Be warned, I never said this was good code ;-)
-  @dates = Dir[settings.data_root + '/*.json'].sort.reverse.first(72).reverse.map { |a| a[/\d{4}-\d{2}-\d{2}-\d*(?=\.)/] }.compact
+  @dates = Dir[settings.data_root + '/*.json'].sort.reverse.first(168).reverse.map { |a| a[/\d{4}-\d{2}-\d{2}-\d*(?=\.)/] }.compact
   erb :index
 end
